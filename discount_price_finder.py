03300d5b20b839580768e8d68fb557c1ced63f6b
Fullzaih <ethaniel.rysz@gmail.com>
2021-04-29 00:39:40 +0000

    Added tax calculations

--- discount_price_finder.py
+++ discount_price_finder.py
@@ -4,5 +4,9 @@ print("Input discount")
 D = int(input())
 n = 100-D
 g = P*n/100
-print(n)
-print(g) 
+print("How much is the sales tax in %")
+i = int(input())
+t = i+100
+f = t*g
+F = f/100
+print("The final price is $", F)
